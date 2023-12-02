68c1913827a0aae1ce6da104417e109b31b37815
Sam Beals <samebeals@yahoo.com>
2023-12-02 17:46:13 -0500

    options

---
+++ app/src/main/java/edu/gvsu/cis/googleadshowcase/BannerActivity.kt
@@ -0,0 +1,11 @@
+package edu.gvsu.cis.googleadshowcase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BannerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_banner)
+    }
+}
