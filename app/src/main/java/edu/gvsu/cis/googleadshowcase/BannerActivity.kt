3274e56481623462ce1e7e8f71bb172a8cd7936f
Sam Beals <samebeals@yahoo.com>
2023-12-03 18:13:41 -0500

    loading banner ad

--- app/src/main/java/edu/gvsu/cis/googleadshowcase/BannerActivity.kt
+++ app/src/main/java/edu/gvsu/cis/googleadshowcase/BannerActivity.kt
@@ -2,10 +2,18 @@ package edu.gvsu.cis.googleadshowcase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class BannerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_banner)
+        MobileAds.initialize(this) {}
+
+        val banner1 = findViewById<AdView>(R.id.banner1)
+        val banner1Request = AdRequest.Builder().build()
+        banner1.loadAd(banner1Request)
     }
 }
--- app/src/main/java/edu/gvsu/cis/googleadshowcase/MainActivity.kt
+++ app/src/main/java/edu/gvsu/cis/googleadshowcase/MainActivity.kt
@@ -1,13 +1,24 @@
 package edu.gvsu.cis.googleadshowcase
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.MobileAds
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         MobileAds.initialize(this) {}
+
+        var bannerBtn = findViewById<Button>(R.id.nav_banners)
+        bannerBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@MainActivity, BannerActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
