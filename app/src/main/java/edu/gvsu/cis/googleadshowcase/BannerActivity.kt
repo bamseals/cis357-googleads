1db0544ec6cd9f6f0cd9c5bfef0b66fe76d88b7c
Sam Beals <samebeals@yahoo.com>
2023-12-04 20:14:30 -0500

    several banners

--- app/src/main/java/edu/gvsu/cis/googleadshowcase/BannerActivity.kt
+++ app/src/main/java/edu/gvsu/cis/googleadshowcase/BannerActivity.kt
@@ -2,9 +2,7 @@ package edu.gvsu.cis.googleadshowcase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.*
 
 class BannerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +10,23 @@ class BannerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_banner)
         MobileAds.initialize(this) {}
 
+        // Standard Banner
         val banner1 = findViewById<AdView>(R.id.banner1)
         val banner1Request = AdRequest.Builder().build()
         banner1.loadAd(banner1Request)
+
+        // Large Banner
+        val banner2 = findViewById<AdView>(R.id.banner2)
+        val banner2Request = AdRequest.Builder().build()
+        banner2.loadAd(banner2Request)
+
+        // Smart Banner
+        val banner3 = findViewById<AdView>(R.id.banner3)
+        val banner3Request = AdRequest.Builder().build()
+        banner3.loadAd(banner3Request)
+
+
+
+
     }
 }
